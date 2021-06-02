5d6bf9ba70dea0a8681b554ef093821e3b3ba6df
ykay220 <81273310+ykay220@users.noreply.github.com>
2021-06-02 18:33:00 -0400

    fixed typo

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,7 +11,7 @@ function Banner() {
                 <h1>Hello!</h1>
                 <p>I'm Yama Karimi, a  developer that loves to code for fun and create visually appealing products. I build responsive web apps mainly in the react framework, and watch all things mma in my spare time!</p>
 
-                <p> I've had the chance to expand my skillset by developing website for businesses.I'm an eager learner that's alway upto date in the newest tech and framework!</p>
+                <p> I've had the chance to expand my skillset by developing websites for businesses.I'm an eager learner that's alway up to date in the newest tech and frameworks!</p>
 
 
             </div>
