003d0564c5a02373e1941907de853cd2ea4d072e
ykay220 <81273310+ykay220@users.noreply.github.com>
2021-06-02 18:16:00 -0400

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ function Banner() {
             </div>
             <div className="banner-text">
                 <h1>Hello!</h1>
-                <p>I'm Yama Karimi, a  developer that loves to code for fun, and create visually appealing products. I build responsive web apps mainly in the frame-work react, and watch all tings mma in my spare time!</p>
+                <p>I'm Yama Karimi, a  developer that loves to code for fun and create visually appealing products. I build responsive web apps mainly in the react framework, and watch all things mma in my spare time!</p>
 
                 <p> I've had the chance to expand my skillset by developing for shops.I'm an eager learner that's alway upto date in the newest tech and framework and!</p>
 
